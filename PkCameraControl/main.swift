91627a63c1e5a500edbe06c91908232593ac1076
Greg <greg@earth-air.local>
2019-03-14 19:19:08 -0400

    Initial Commit

---
+++ PkCameraControl/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PkCameraControl
+//
+//  Created by Greg on 2019-03-14.
+//  Copyright © 2019 Greg Beaton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
